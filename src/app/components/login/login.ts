f99fcf88342d7287d3206f1d688577f0f64e3250
lucas_hbtec <lucas.souza@hbtec.com.br>
2025-06-20 17:52:00 -0300

    adicionado logica de criptografia seguindo o mesmo padrão

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-login',
@@ -13,28 +14,37 @@ import { HttpClient } from '@angular/common/http';
 export class Login {
 
     private http = inject(HttpClient);
+    private auth = inject(AuthService);
     
     loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
-      password: new FormControl('fYiIFGZxR5uf2/78HE3ON48bs3D45L3Y7ae1LDPLuURYv6uYglBpfeVE5W5KzuElTYLFoYMh1ClZrg6o+JR/HqkHDk5MhX1in3CWFUeXtfHgcXlEGlRdJ6AEfgjmPNZfCEwP3ic/YYmreyOBQ6BhDhISgLAgzr8PBWMoHL2AKBWwhBLfJkzrk/zF3k8C6EJC8Bcf3QaYzJiZmczwGS4A00E9mv1h+oxD30mtbrDvTfljhRBf1+cLFUjv91QNABkhdjloDHB+oS6RVjNaBRLeIvXV5raGrydW4A5CE+senIYOGaRiNRGdMO+Wf8n9pp/qQe9lMWSJAzuHyzrmX9BeeQ==', Validators.required),
+      password: new FormControl('', Validators.required),
       opdusCods: new FormControl(null, Validators.required),
       token: new FormControl(null, Validators.required),
       recaptchaResponse: new FormControl('', Validators.required),
     });
 
-    login() {
-      this.http.post('/rede/apirest/users/validaNovo', this.loginForm.value).subscribe((res: any) => {
+    // ✅ Chave pública PEM
+    pemPublicKey = `-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsQecxIHHp8T3P2QRsPGd
+    tCFnAdEpgG0WCFMp36kZQqqSn8q6ZB+89PZ1S6cUp6kUtUyeuI3vOoy1S87f0M2a
+    ojiXAgJ5x7apJhkII5PU3blQpWE+NHxCbRlGbYj435ar+mp22opl8HlDxW9wLyNF
+    biwewmJWGVwqKviur7rJY1r9u+I4KJmjbPB5KeQ4WGxQM7ImCJyEMzL4xgbWPK/5
+    Y2uTrazlpcNFP3jpLekg31H3PytJs+t/YEe43THm8AzW70Af3Q5mEP260mMwxtcX
+    cX3KFLiLcnRsUDVTKUFHAck0HzOhZYCj5CrdDxhvkTVATF0s8OfLxA5rGM7kG8Xs
+    iwIDAQAB
+    -----END PUBLIC KEY-----`;
+
+    async login() {
+      const formValue = this.loginForm.value;
+      const publicKey = await this.auth.importPublicKey(this.pemPublicKey);
+      const encryptedPassword = await this.auth.encryptData(publicKey, formValue.password!);
+      const loginData = {
+        ...formValue,
+        password: encryptedPassword
+      };
+      this.http.post('/rede/apirest/users/validaNovo', loginData).subscribe((res: any) => {
           console.log(res)
       })
-      // const formValue = this.loginForm.value;
-
-      // // Criptografa a senha
-      // const encryptedPassword = this.encriptData(formValue.password);
-
-      // // Cria novo objeto com senha criptografada
-      // const loginData = {
-      //   ...formValue,
-      //   password: encryptedPassword
-      // };
     }
 }
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +7,41 @@ import * as CryptoJS from 'crypto-js';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
+
+  // Função auxiliar para converter PEM para SPKI
+  public pemToSpki(pem: string): ArrayBuffer {
+    const pemHeader = "-----BEGIN PUBLIC KEY-----";
+    const pemFooter = "-----END PUBLIC KEY-----";
+    const pemContents = pem.replace(pemHeader, '').replace(pemFooter, '').replace(/\s+/g, '');
+    const binaryDerString = atob(pemContents);
+    const binaryDer = new Uint8Array(binaryDerString.length);
+    for (let i = 0; i < binaryDerString.length; i++) {
+      binaryDer[i] = binaryDerString.charCodeAt(i);
+    }
+    return binaryDer.buffer;
+  }
+
+  // Importar chave pública
+  public async importPublicKey(pem: string): Promise<CryptoKey> {
+    const spki = this.pemToSpki(pem);
+    return await crypto.subtle.importKey(
+      'spki',
+      spki,
+      {
+        name: 'RSA-OAEP',
+        hash: 'SHA-256'
+      },
+      true,
+      ['encrypt']
+    );
+  }
+
+  // Criptografar dados
+  public async encryptData(publicKey: CryptoKey, data: string): Promise<string> {
+    const encoded = new TextEncoder().encode(data);
+    const encrypted = await crypto.subtle.encrypt({ name: 'RSA-OAEP' }, publicKey, encoded);
+    const encryptedBytes = new Uint8Array(encrypted);
+    return btoa(String.fromCharCode(...encryptedBytes)); // Base64
+  }
   
 }
