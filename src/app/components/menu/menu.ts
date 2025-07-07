c927cc6139eba6101a36714dd65c144f1904c942
lucas_hbtec <lucas.souza@hbtec.com.br>
2025-07-07 18:02:17 -0300

    ajustado css

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,10 +1,11 @@
-import { Component, HostListener, signal } from '@angular/core';
+import { Component, computed, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from "../sidebar/sidebar";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, Sidebar, CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css'
 })
@@ -12,6 +13,14 @@ export class Menu {
 
   isLeftSidebarCollapsed = signal<boolean>(false);
   screenWidth = signal<number>(window.innerWidth);
+
+  sizeClass = computed(() => {
+    const isLeftSidebarCollapsed = this.isLeftSidebarCollapsed();
+    if (isLeftSidebarCollapsed) {
+      return '';
+    }
+    return this.screenWidth() > 768 ? 'body-trimmed' : 'body-md-screen';
+  });
 
   @HostListener('window:resize')
   onResize() {
