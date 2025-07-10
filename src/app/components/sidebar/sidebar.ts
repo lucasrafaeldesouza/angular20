6cff9f6660f36c1b8bbdeac098c8eb2588a3722f
lucas_hbtec <lucas.souza@hbtec.com.br>
2025-07-10 16:25:18 -0300

    sidebar dinamica com os menus vindo da API

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -6,8 +6,8 @@ import { HttpClient } from '@angular/common/http';
 
 interface MenuItem {
   icon: string;
-  label: string;
-  children?: MenuItem[];
+  opaeDsc: string;
+  vestrutAcessoUsuarItens?: MenuItem[];
   isOpen?: boolean;
 }
 
@@ -30,48 +30,40 @@ export class Sidebar {
   isLeftSidebarCollapsed = input.required<boolean>();
   changeIsLeftSidebarCollapsed = output<boolean>();
 
-  items: MenuItem[] = [
-    {
-      icon: 'fal fa-user-headset',
-      label: 'Operadoras',
-      isOpen: false,
-      children: [
-        { icon: 'fas fa-chart-pie', label: 'Clientes' },
-        { icon: 'fas fa-tasks', label: 'Tabela de preços Base' },
-        { icon: 'fas fa-tasks', label: 'Modelos de contratos' },
-        { icon: 'fas fa-tasks', label: 'Histórico de atualizações' },
-        { icon: 'fas fa-tasks', label: 'Histórico de interações' },
-      ],
-    },
-    {
-      icon: 'fal fa-stethoscope',
-      label: 'Prestadores',
-      children: [
-        { icon: 'fas fa-chart-pie', label: 'Clientes' },
-        { icon: 'fas fa-tasks', label: 'Tabela de preços Base' },
-        { icon: 'fas fa-tasks', label: 'Modelos de contratos' },
-        { icon: 'fas fa-tasks', label: 'Histórico de atualizações' },
-        { icon: 'fas fa-tasks', label: 'Coeficientes da dimensionamento' },
-        { icon: 'fas fa-chart-pie', label: 'Repositório' },
-        { icon: 'fas fa-tasks', label: 'Comunicados' },
-        { icon: 'fas fa-tasks', label: 'Ocorrências' },
-        { icon: 'fas fa-tasks', label: 'Atas de reuniões' },
-        { icon: 'fas fa-tasks', label: 'Agenda' },
-      ],
-    },
-    {
-      icon: 'fal fa-users',
-      label: 'Relacionamento',
-    },
-    {
-      icon: 'fal fa-cog',
-      label: 'Configurações',
-    },
-  ];
+  items: MenuItem[] = [];
 
   buscaItensMenu(opdusCod: number | undefined) {
     this.http.get('/rede/apirest/controle_link_view/carregaVEstrutAcessoUsuar/'+opdusCod +'/'+"'M'"+'/'+null+'/'+null).subscribe((res: any) => {
       console.log(res)
+      res.forEach(function(objeto: any) {
+        switch (objeto.opaeDsc) {
+          case "Cadastro":
+            objeto.icon = "fal fa-plus"
+            break;
+          case "Administrador":
+            objeto.icon = "fal fa-user-tie"
+            break;
+          case "Operadora":
+            objeto.icon = "fal fa-user-headset"
+          break;
+          case "Prestador":
+            objeto.icon = "fal fa-stethoscope"
+          break;
+          case "Relacionamento":
+            objeto.icon = "fal fa-users"
+          break;
+          case "Dimensionamento":
+            objeto.icon = "fal fa-vector-square"
+          break;
+          case "Qualificação":
+            objeto.icon = "fal fa-user-minus"
+          break;
+          case "Plano de Ação":
+            objeto.icon = "fal fa-list-ol"
+          break;
+        }
+      });
+      this.items = res
     })
   }
 
@@ -84,8 +76,7 @@ export class Sidebar {
   }
 
   toggleMenuItem(item: MenuItem) {
-    if (!this.isLeftSidebarCollapsed() && item.children) {
-      console.log(item)
+    if (!this.isLeftSidebarCollapsed() && item.vestrutAcessoUsuarItens) {
       item.isOpen = !item.isOpen;
     }
   }
